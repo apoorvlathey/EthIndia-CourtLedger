77fd57a2b6db4e1e175cab926e1ef04d07341079
CodinMaster <latheyapoorv@gmail.com>
2020-02-29 20:05:45 +0530

    deployed to matic network

--- client/src/middleware/web3.js
+++ client/src/middleware/web3.js
@@ -1,31 +1,32 @@
-import BetaAPI from './SampleBetaApp.json';
-import Web3 from 'web3';
+import BetaAPI from "./SampleBetaApp.json";
+import Web3 from "web3";
 var web3;
 
-  if (window.ethereum) {
-      web3 = new Web3(window.ethereum);
+if (window.ethereum) {
+  web3 = new Web3(window.ethereum);
 
-      try {
-          // Request account access if needed
-          window.ethereum.enable()
-          .then(console.log('Acccounts now exposed'))
-          .catch(error => console.log(error));
-      } catch (error) {
-          console.log(error.message);
-      }
-
+  try {
+    // Request account access if needed
+    window.ethereum
+      .enable()
+      .then(console.log("Acccounts now exposed"))
+      .catch(error => console.log(error));
+  } catch (error) {
+    console.log(error.message);
   }
-
-  else if(typeof window !== 'undefined' && typeof window.web3 !== 'undefined'){
-      web3 = new Web3(window.web3.currentProvider);
-  }
-  
-  const instance = new web3.eth.Contract(
-    BetaAPI,
-     '0xdBa8EC446B8fa878EE6d0d0C4A73995Ac7486706'
-  );
-  
-  export default {
-    Config : instance,
-    web3 : web3
+} else if (
+  typeof window !== "undefined" &&
+  typeof window.web3 !== "undefined"
+) {
+  web3 = new Web3(window.web3.currentProvider);
 }
+
+const instance = new web3.eth.Contract(
+  BetaAPI,
+  "0x9B165c7a931214e227D43F39EDDE57acCf107261"
+);
+
+export default {
+  Config: instance,
+  web3: web3
+};
--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 var HDWalletProvider = require("@truffle/hdwallet-provider");
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -11,13 +11,24 @@ module.exports = {
       port: 8545
     },
     kovan: {
-      provider: function () {
+      provider: function() {
         return new HDWalletProvider(
           process.env.MNEMONIC,
           "https://kovan.infura.io/v3/d6567ea751014dc6b2426e02cd9ac714"
         );
       },
       network_id: 42,
+      gas: 4500000,
+      gasPrice: 10000000000
+    },
+    matic: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://testnet2.matic.network"
+        );
+      },
+      network_id: 8995,
       gas: 4500000,
       gasPrice: 10000000000
     }
